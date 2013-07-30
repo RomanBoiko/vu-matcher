a719567158d1a324f0d03577dcaacfa68151e25d
Jon Eland <jonathan.eland@gmail.com>
2013-07-30 23:06:16 +0100

    Introduced midje; Added facts about cluster-using-group-by; Removed one level of nesting in conditions namespace

--- src/matcher/conditions.clj
+++ src/matcher/conditions.clj
@@ -1,13 +1,7 @@
 (ns matcher.conditions (:gen-class))
 
-(defn cluster_using_group_by [condition records]
-  (vals (dissoc (group-by condition records) nil)))
-
-(defn apply-cluster [cluster-function condition list-of-clusters]
-  (apply concat (map (partial cluster-function condition) list-of-clusters)))
-
+(defn cluster-using-group-by [key records]
+  (vals (dissoc (group-by #(get % key) records) nil)))
 
 (defn condition-values-equal [key list-of-clusters]
-  (apply-cluster cluster_using_group_by #(get % key) list-of-clusters))
-
-
+  (apply concat (map (partial cluster-using-group-by key) list-of-clusters)))
---
+++ test/matcher/conditions_midje_test.clj
@@ -0,0 +1,20 @@
+(ns matcher.conditions-midje-test
+  (:require [midje.sweet :refer :all]
+            [matcher.conditions :refer :all]))
+
+(facts "about `cluster-using-group-by`"
+  (fact "will group maps within same vector that have matching value for given key"
+    (cluster-using-group-by "k1" [{"k1" "v1"}]) => [[{"k1" "v1"}]]
+    (cluster-using-group-by "k1" [{"k1" "v1"} {"k1" "v1"}]) => [[{"k1" "v1"} {"k1" "v1"}]])
+
+  (fact "will group maps in different vectors that do not have matching value for given key"
+    (cluster-using-group-by "k1" [{"k1" "v1"} {"k1" "v2"}]) => [[{"k1" "v1"}] [{"k1" "v2"}]]
+    (cluster-using-group-by "k1" [{"k1" "v1"} {"k1" "v2"} {"k1" "v3"}]) => [[{"k1" "v1"}] [{"k1" "v2"}] [{"k1" "v3"}]])
+
+  (fact "will return nil when none of the maps contain given key"
+    (cluster-using-group-by "k1" [{}]) => nil
+    (cluster-using-group-by "k1" [{"k2" "v2"}]) => nil
+    (cluster-using-group-by "k1" [{"k2" "v2"} {}]) => nil)
+
+  (fact "will group maps when there is at least one matching value for given key"
+    (cluster-using-group-by "k1" [{"k1" "v1"} {}]) => [[{"k1" "v1"}]]))
