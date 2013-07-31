7bc620db3801ec6a6450d1d5e448214706444555
Jon Eland <jonathan.eland@gmail.com>
2013-07-31 21:49:37 +0100

    Added final case for midje unit test

--- test/matcher/conditions_midje_test.clj
+++ test/matcher/conditions_midje_test.clj
@@ -18,3 +18,13 @@
 
   (fact "will group maps when there is at least one matching value for given key"
     (cluster-using-group-by "k1" [{"k1" "v1"} {}]) => [[{"k1" "v1"}]]))
+
+(facts "about `condition-values-equal`"
+  (fact "will only group matches that are part of the same vector"
+    (condition-values-equal "k1" [[{"k1" "v1"} {"k1" "v1"}]]) => [[{"k1" "v1"} {"k1" "v1"}]]
+    (provided 
+      (cluster-using-group-by "k1" [{"k1" "v1"} {"k1" "v1"}]) => [[{"k1" "v1"} {"k1" "v1"}]])
+    
+    (condition-values-equal "k1" [[{"k1" "v1"}] [{"k1" "v1"}]]) => [[{"k1" "v1"}] [{"k1" "v1"}]]
+    (provided 
+      (cluster-using-group-by "k1" [{"k1" "v1"}]) => [[{"k1" "v1"}]] :times 2)))
