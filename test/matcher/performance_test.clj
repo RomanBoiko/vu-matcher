35b3447d5c20aff16bd7d2b58d840abeb69f2a35
Jon Eland <jonathan.eland@gmail.com>
2013-08-01 21:46:24 +0100

    Renamed performance function

--- test/matcher/performance_test.clj
+++ test/matcher/performance_test.clj
@@ -4,13 +4,13 @@
     [matcher.generator :refer :all]
     [midje.sweet :refer :all]))
 
-(defn timestamp-wrapper [key list-of-clusters]
+(defn performance-wrapper [key list-of-clusters]
   (println "Number of maps in sequence:" (count list-of-clusters)". Size of maps:" (count (first list-of-clusters)))
   (time (condition-values-equal key list-of-clusters)))
 
 (fact "performance test measuring elapsed time of processing"
-  (timestamp-wrapper "k1" (generate-sequence-of-maps 50 5)) => ()
-  (timestamp-wrapper "k1" (generate-sequence-of-maps 500 5)) => ()
-  (timestamp-wrapper "k1" (generate-sequence-of-maps 1000 5)) => ()
-  (timestamp-wrapper "k1" (generate-sequence-of-maps 10000 10)) => ()
-  (timestamp-wrapper "k1" (generate-sequence-of-maps 100000 10)) => ())
+  (performance-wrapper "k1" (generate-sequence-of-maps 50 5)) => ()
+  (performance-wrapper "k1" (generate-sequence-of-maps 500 5)) => ()
+  (performance-wrapper "k1" (generate-sequence-of-maps 1000 5)) => ()
+  (performance-wrapper "k1" (generate-sequence-of-maps 10000 10)) => ()
+  (performance-wrapper "k1" (generate-sequence-of-maps 100000 10)) => ())
