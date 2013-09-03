4e2664d1a136e18e2ce536df5e13d66c3118e320
boikoro <boiko.roman@gmail.com>
2013-09-04 00:35:33 +0100

    [conditions] new condition - leave only groups that have exactly given number of records

--- src/matcher/conditions.clj
+++ src/matcher/conditions.clj
@@ -3,5 +3,8 @@
 (defn cluster-using-group-by [key records]
   (vals (dissoc (group-by #(get % key) records) nil)))
 
-(defn condition-values-equal [key list-of-clusters]
+(defn values-equal [key list-of-clusters]
   (mapcat (partial cluster-using-group-by key) list-of-clusters))
+
+(defn number-of-records-in-group-is [records-number list-of-clusters]
+  (filter #(= (count %) records-number) list-of-clusters))
--- test/matcher/conditions_test.clj
+++ test/matcher/conditions_test.clj
@@ -20,12 +20,20 @@
   (fact "will group maps when there is at least one matching value for given key"
     (cluster-using-group-by "k1" [{"k1" "v1"} {}]) => [[{"k1" "v1"}]]))
 
-(facts "about `condition-values-equal`"
+(facts "about `values-equal`"
   (fact "will only group matches that are part of the same vector"
-    (condition-values-equal "k1" [[{"k1" "v1"} {"k1" "v1"}]]) => [[{"k1" "v1"} {"k1" "v1"}]]
-    (provided 
+    (values-equal "k1" [[{"k1" "v1"} {"k1" "v1"}]]) => [[{"k1" "v1"} {"k1" "v1"}]]
+    (provided
       (cluster-using-group-by "k1" [{"k1" "v1"} {"k1" "v1"}]) => [[{"k1" "v1"} {"k1" "v1"}]])
-    
-    (condition-values-equal "k1" [[{"k1" "v1"}] [{"k1" "v1"}]]) => [[{"k1" "v1"}] [{"k1" "v1"}]]
-    (provided 
+
+    (values-equal "k1" [[{"k1" "v1"}] [{"k1" "v1"}]]) => [[{"k1" "v1"}] [{"k1" "v1"}]]
+    (provided
       (cluster-using-group-by "k1" [{"k1" "v1"}]) => [[{"k1" "v1"}]] :times 2)))
+
+(facts "about `number-of-records-in-group-is`"
+  (fact "will create matches only if number of records in group is as given"
+    (number-of-records-in-group-is 2 [[{} {}]]) => [[{} {}]]
+    (number-of-records-in-group-is 2 [[{}]]) => []
+    (number-of-records-in-group-is 2 [[{} {}] [{}]]) => [[{} {}]]
+    (number-of-records-in-group-is 3 [[{} {} {}][{} {}]]) => [[{} {} {}]]
+    (number-of-records-in-group-is 1 [[{} {}] [{}]]) => [[{}]]))
