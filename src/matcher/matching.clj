b43c6d6fa986ec291790f9032a072aa37314f3dc
boikoro <boiko.roman@gmail.com>
2013-09-05 01:02:45 +0100

    [matching] matching fully integrated, matching occurs regularly and results now stored to db

--- src/matcher/matching.clj
+++ src/matcher/matching.clj
@@ -1,6 +1,7 @@
 (ns matcher.matching
   (:require [taoensso.timbre :as log]
-            [matcher.db :as db]))
+            [matcher.db :as db]
+            [matcher.rules :refer :all]))
 
 (defn err-handler-fn [ag ex]
     (log/error (str "error occured in matching agent " ag": " ex)))
@@ -8,8 +9,13 @@
 (def matching-agent (agent 0 :error-handler err-handler-fn))
 
 (defn match-active-records []
-  (let [active-records (db/all-active-records)]
-    1))
+  (let [active-records (db/all-active-records)
+        match-results (ruleset [perfect-match-demo-rule suggested-match-demo-rule] active-records)]
+    (doseq [match-type match-results]
+      (let [match-name (first (keys match-type))]
+        (doseq [group-records (first (vals match-type))]
+          (log/info (str "Storing match in db: " match-name "(" group-records ")"))
+          (db/convert-active-records-to-matched-group match-name group-records))))))
 
 (defn matching-process [agent-state]
   (loop []
